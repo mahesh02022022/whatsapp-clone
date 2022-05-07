71e041d1863001f7b4b05dd45de99ded7ca93034
mahemall <free.ibm@gmail.com>
2022-05-08 02:03:44 +0530

    Initial commit

---
+++ precache-manifest.68d5cf18b1fbfab158e20cd5756e446a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90385916a7032e7fc1fa3b53b0ef3f0e",
+    "url": "/whatsapp-clone/index.html"
+  },
+  {
+    "revision": "ca3f541e4174c8602bfe",
+    "url": "/whatsapp-clone/static/css/main.9e8185c7.chunk.css"
+  },
+  {
+    "revision": "fc980388f20d90ebc6f1",
+    "url": "/whatsapp-clone/static/js/2.ed1b6a88.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/whatsapp-clone/static/js/2.ed1b6a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca3f541e4174c8602bfe",
+    "url": "/whatsapp-clone/static/js/main.8f750af9.chunk.js"
+  },
+  {
+    "revision": "8d0a8cdab6927a1f4f08",
+    "url": "/whatsapp-clone/static/js/runtime-main.a04c35f0.js"
+  }
+]);
